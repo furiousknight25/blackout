604e0b86ce341fc3e0e8be3806f62e4c537c334a
furiousknight25 <48182589+furiousknight25@users.noreply.github.com>
2025-08-07 18:36:35 -0500

    latik

--- scripts/generator.gd
+++ scripts/generator.gd
@@ -2,7 +2,7 @@ extends Node
 @onready var progress_bar: ProgressBar = $Sprite3D/SubViewport/ProgressBar
 @onready var idle_loop_sfx: AudioStreamPlayer3D = $IdleLoopSFX
 
-var total_power = 100
+var total_power = 30
 var delta_power_decrease = -5
 var delta_power_increase = 20
 var timer : Timer
@@ -11,6 +11,7 @@ var incrementTimer : float = 1.5 #time it takes to increase a chunk of progress
 var percentIncrease : int = 20 #the amount of progress in percentage is increased
 var currentIncrementTime : float = 0 #The amount of time held down so far
 
+signal power_changed(current_power)
 
 func _ready() -> void:
 	timer = Timer.new()
@@ -27,6 +28,7 @@ func _on_timer_timeout(): #reducing power on here
 	total_power = clampf(total_power + delta_power_decrease, 0.0, 100.0)
 	
 	progress_bar.value = total_power
+	emit_signal("power_changed", total_power)
 	print(total_power)
 	
 
