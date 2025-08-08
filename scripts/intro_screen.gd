beccc1317fe2f24ecb572db1bbd4082ce1259694
Phoenix Jordan <136138123+BirdOfAsh@users.noreply.github.com>
2025-08-07 22:48:13 -0500

    I'm a goober just fixed something

--- scripts/intro_screen.gd
+++ scripts/intro_screen.gd
@@ -18,4 +18,4 @@ func transition():
 		animation_player.play("FadeOut")
 		
 		await animation_player.animation_finished
-		get_tree().change_scene_to_file("res://scenes/TestSceneOne.tscn")
+		get_tree().change_scene_to_file("res://scenes/RealMainScene.tscn")
