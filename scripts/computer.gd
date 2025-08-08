cc8a9f41513e2d097e9c032a2b1a13eb556d3fe4
Phoenix Jordan <136138123+BirdOfAsh@users.noreply.github.com>
2025-08-07 19:42:15 -0500

    Added color change on typing the correct sequence of numbers

--- scripts/computer.gd
+++ scripts/computer.gd
@@ -2,20 +2,67 @@ class_name Computer
 extends StaticBody3D
 
 @onready var progress_bar: ProgressBar = $Sprite3D/SubViewport/ProgressBar
-@onready var ui_popup: UIPopup = $"UI Popup"
+@onready var num_label: Label3D = $NumLabel
+@onready var rich_text_label: RichTextLabel = $Numbers/SubViewport/RichTextLabel
 
 
 var incrementTimer : float = 2.0 #time it takes to increase a chunk of progress
 var percentIncrease : int = 10 #the amount of progress in percentage is increased
 var currentIncrementTime : float = 0 #The amount of time held down so far
 
+var keysPressed = ""
+var randNumString = ""
 
-func interact(delta : float):
-	currentIncrementTime += delta
-	if currentIncrementTime >= incrementTimer and progress_bar.value < 100:
-		progress_bar.value += percentIncrease
-		currentIncrementTime = 0
+
+func _ready() -> void:
+	randomizeNumString()
+	
+	SignalBus.connect("hideUI", hideUI)
+
+
+func interact(_delta : float):
+		pass
+
+
+func _input(event: InputEvent) -> void:
+	if rich_text_label.visible:
+		if event is InputEventKey and event.is_pressed() and event.physical_keycode >= KEY_0 and event.physical_keycode <= KEY_9:
+			keysPressed = keysPressed + char(event.physical_keycode)
+			checkForMatch()
+			resetKeys()
+
+
+func resetKeys():
+	if keysPressed.length() >= 4:
+		keysPressed = ""
+
+
+func checkForMatch(): 
+	updateColoration()
+	if keysPressed == randNumString:
+		progress_bar.value = clamp(progress_bar.value + percentIncrease, 0, 100)
+		randomizeNumString()
+
+
+func randomizeNumString():
+	randNumString = ""
+	for i in range(0, 4):
+		randNumString = randNumString + str(randi_range(0, 9))
+	rich_text_label.text = "[outline_size=5]" + "[outline_color=black]" + randNumString + "[/outline_color]" + "[/outline_size]"
+	
+	keysPressed = ""
+
+
+func updateColoration() -> void:
+	if randNumString.contains(keysPressed):
+		rich_text_label.text = "[outline_size=5]" + "[outline_color=black]" + "[color=lime]" + randNumString.substr(0,keysPressed.length()) + "[/color]" + randNumString.substr(keysPressed.length(), randNumString.length()) + "[/outline_color]" + "[/outline_size]"
+	else:
+		randomizeNumString()
 
 
 func showUI():
-	ui_popup.fadeIn()
+	rich_text_label.visible = true
+
+
+func hideUI():
+	rich_text_label.visible = false
