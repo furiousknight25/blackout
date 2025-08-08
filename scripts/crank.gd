62e554f53b61bbfd413c96fc9422da6da5bea14b
furiousknight25 <48182589+furiousknight25@users.noreply.github.com>
2025-08-07 20:35:15 -0500

    a lot more

---
+++ scripts/crank.gd
@@ -0,0 +1,18 @@
+extends MeshInstance3D
+
+@onready var timer: Timer = $Timer
+@onready var crank_sound: AudioStreamPlayer3D = $CrankSound
+
+var last_rotation = Vector3.ZERO
+
+func _ready() -> void:
+	crank_sound.stream_paused = true
+	
+func _process(delta: float) -> void:
+	if last_rotation != rotation:
+		last_rotation = rotation
+		crank_sound.stream_paused = false
+		timer.start()
+	
+func _on_timer_timeout() -> void:
+	crank_sound.stream_paused = true
