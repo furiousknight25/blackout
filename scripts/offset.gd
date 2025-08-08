030eab609413e33d48466feff2811da84d8d3272
Phoenix Jordan <136138123+BirdOfAsh@users.noreply.github.com>
2025-08-07 19:46:29 -0500

    Fixed a small debug error

--- scripts/offset.gd
+++ scripts/offset.gd
@@ -1,4 +1,4 @@
 extends Marker3D
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	rotation = $Camera3D/Rig/fpsanimations/ArmRIG/Skeleton3D/BoneAttachment3D.rotation
