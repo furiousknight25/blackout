4c340f66cc8fd2ec2f0ea079ea21703058e1b0b9
Aria-Williams <159070680+Area9579@users.noreply.github.com>
2025-08-08 22:13:32 -0500

    fixed main menu

--- main_menu.gd
+++ main_menu.gd
@@ -3,20 +3,25 @@ extends Node2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var play_button: TextureButton = $PlayButton
 @onready var coverup_rect: ColorRect = $coverupRect
-
+@onready var music_sfx: AudioStreamPlayer2D = $MusicSFX
+@onready var fade: AnimationPlayer = $Fade
+@onready var fade_2: ColorRect = $Fade2
 
 func _ready() -> void:
+	fade.play("fadeIn")
+	music_sfx.volume_db = -80.0
+	music_sfx.playing = true
 	animation_player.play("Sway")
+	
 
-
+func _process(delta: float) -> void:
+	music_sfx.volume_db = lerp(music_sfx.volume_db, 0.0, delta * 1.5)
 
 func transition():
-	if animation_player.current_animation != "FadeOut":
-		animation_player.play("FadeOut")
-		
-		await animation_player.animation_finished
-		get_tree().change_scene_to_file.bind("res://scenes/intro_screen.tscn").call_deferred()
-		#get_tree().change_scene_to_file("res://scenes/intro_screen.tscn")
+	fade.play("fadeOut")
+	await fade.animation_finished
+	get_tree().change_scene_to_file.bind("res://scenes/intro_screen.tscn").call_deferred()
+	#get_tree().change_scene_to_file("res://scenes/intro_screen.tscn")
 
 
 func _on_play_button_button_up() -> void:
@@ -25,3 +30,7 @@ func _on_play_button_button_up() -> void:
 
 func _on_quit_button_button_up() -> void:
 	get_tree().quit()
+
+
+func _on_fade_animation_finished(anim_name: StringName) -> void:
+	fade_2.color = Color.TRANSPARENT
