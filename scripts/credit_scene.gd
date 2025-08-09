ea16936dce0da6c93164e69ea3b05246c7dfe18e
Aria-Williams <159070680+Area9579@users.noreply.github.com>
2025-08-08 22:10:56 -0500

    updated credit scene

--- scripts/credit_scene.gd
+++ scripts/credit_scene.gd
@@ -2,10 +2,20 @@ extends Node2D
 
 @onready var background_player: AnimationPlayer = $BackgroundPlayer
 @onready var credit_scroll: AnimationPlayer = $CreditScroll
+@onready var music_sfx: AudioStreamPlayer2D = $MusicSFX
+@onready var fade: AnimationPlayer = $Fade
+
 
 func _ready() -> void:
+	
+	music_sfx.volume_db = -80.0
+	music_sfx.playing = true
 	background_player.play("background movement")
 	credit_scroll.play("scroll")
+	fade.play("fadeIn")
+
+func _process(delta: float) -> void:
+	music_sfx.volume_db = lerp(music_sfx.volume_db, 0.0, delta * 1.5)
 
 func _on_background_player_animation_finished(anim_name: StringName) -> void:
 	background_player.play("background movement")
